7ad443996175628c409a220cda25169da593b4b0
Sami Cenkci <121030749+SamiCenkci@users.noreply.github.com>
2026-07-19 13:11:07 +0200

    Add FAQ chatbot helper

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import ChatBot from "@/components/ChatBot";
+import ChatBot from "@/components/Chatbot";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
